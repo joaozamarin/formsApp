3496125b8bba3a6906e446cac8bfddc5de826347
João Vitor Zamarin <joao.zamarin@gmail.com>
2022-11-07 22:27:06 -0300

    form registro validação

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formRegistro = this.formBuilder.group({
+    nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    email: ['', Validators.compose([Validators.required ,Validators.email, Validators.minLength(6)])],
+    cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(11)])],
+    senha: [],
+    confirmaSenha: []
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
