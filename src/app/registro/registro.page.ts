e5b8c74c88e142a262d3ae2171fb2de58967a9e1
João Vitor Zamarin <joao.zamarin@gmail.com>
2022-11-11 21:26:46 -0300

    validação registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -12,11 +12,44 @@ export class RegistroPage implements OnInit {
     nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
     email: ['', Validators.compose([Validators.required ,Validators.email, Validators.minLength(6)])],
     cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(11)])],
-    senha: [],
-    confirmaSenha: []
+    senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+    confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
   });
 
+  mensagensErro = {
+    nome: [
+      {tipo: 'required', aviso: 'Insira um Nome!'},
+      {tipo: 'minLength', aviso: 'O nome deve ter, no mínimo, 3 caracteres'}
+    ],
+
+    email: [
+      {tipo: 'required', aviso: 'Insira um E-mail!'},
+      {tipo: 'email', aviso: 'Insura um E-mail válido!'}
+    ],
+
+    cpf: [
+      {tipo: 'required', aviso: 'Insira um CPF!'},
+      {tipo: 'minLength', aviso: 'Insira um CPF válido!'},
+      {tipo: 'maxLength', aviso: 'Insira um CPF válido!'}
+    ],
+
+    senha: [
+      {tipo: 'required', aviso: 'Insira uma senha!'},
+      {tipo: 'minLength', aviso: 'A senha deve conter, no mínimo, 6 caracteres!'}
+    ],
+
+    confirmaSenha: [
+      {tipo: 'required', aviso: 'Insira uma senha!'},
+      {tipo: 'minLength', aviso: 'A senha deve conter, no mínimo, 6 caracteres!'}
+    ]
+  };
+
   constructor(private formBuilder: FormBuilder) { }
+
+  get nome()
+  {
+    return this.formRegistro.get('nome');
+  }
 
   ngOnInit() {
   }
